4515ebe914cfc44e412d09a6d49d556c2f102a9e
Luís Gustavo Pereira <lgp1985@outlook.com>
2025-09-14 21:19:30 -0300

    Update build process and implement GPIO functionality
    Modified `Lgp.Rf433.csproj` to enhance the `PostBuild` command for copying files and added a reference to `System.Device.Gpio` version `4.0.1`.
    
    In `Program.cs`, replaced initial console output with a debugging mechanism, implemented GPIO functionality, and set up a timer and callback for handling pin value changes.

--- Lgp.Rf433/Program.cs
+++ Lgp.Rf433/Program.cs
@@ -1,2 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Device.Gpio;
+using System.Timers;
+
+while (!System.Diagnostics.Debugger.IsAttached)
+{
+    Thread.Sleep(100);
+}
+Console.WriteLine("debugger attached");
+System.Diagnostics.Debugger.Break();
+
+const int Pin = 2;
+
+using var controller = new GpioController();
+
+using var pin = controller.OpenPin(Pin, PinMode.Input);
+
+
+List<byte> bytes = [];
+
+int bufferTimeout = 500;
+
+using System.Timers.Timer timer = new(bufferTimeout) { AutoReset = true };
+timer.Elapsed += TimerCallback;
+
+void TimerCallback(object? sender, ElapsedEventArgs e)
+{
+    if (bytes.Count == 0) return;
+    Console.WriteLine(Convert.ToBase64String(bytes.ToArray()));
+    bytes = [];
+}
+
+byte byteCount = 0;
+
+void OnPinEvent(object sender, PinValueChangedEventArgs args)
+{
+    timer.Stop(); timer.Start();
+    switch (args.ChangeType)
+    {
+        case PinEventTypes.None:
+            Console.WriteLine("none");
+            break;
+        case PinEventTypes.Rising:
+            bytes.Add(byteCount);
+            byteCount = 0;
+            break;
+        case PinEventTypes.Falling:
+            byteCount++;
+            break;
+        default:
+            Console.WriteLine("default");
+            break;
+    }
+}
+
+controller.RegisterCallbackForPinValueChangedEvent(
+    Pin,
+    PinEventTypes.Falling | PinEventTypes.Rising,
+    OnPinEvent);
+
+await Task.Delay(Timeout.Infinite);
